2e2cacde697fb769992d0308be5f3a7a59338dfe
Igor Pekarskij <igorpekarskij.brest@gmail.com>
2016-11-30 01:26:59 +0300

    Setted  min dimension for login window

--- Organizer/src/sample/Main.java
+++ Organizer/src/sample/Main.java
@@ -10,9 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../fxmls/sample.fxml"));
+        primaryStage.setTitle("Organizer");
+        primaryStage.setMinHeight(230);
+        primaryStage.setMinWidth(610);
+        primaryStage.setScene(new Scene(root, 220, 275));
         primaryStage.show();
     }
 
