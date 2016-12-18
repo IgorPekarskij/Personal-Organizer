3583c08249a4d6fb8bbf16632cae39903de50ff6
Igor Pekarskij <igorpekarskij.brest@gmail.com>
2016-12-18 20:51:14 +0300

    Added method to set Image to contact, method that sets person detail information. Fixed cancel save contact method. Added method hideWindow. Rename some methods.

--- Organizer/src/main/java/controllers/CreateNewContactController.java
+++ Organizer/src/main/java/controllers/CreateNewContactController.java
@@ -2,19 +2,26 @@ package controllers;
 
 import interfaces.impls.CollectionContacts;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import objects.Person;
+import utils.ConvertData;
+import java.io.File;
 
 public class CreateNewContactController {
-@FXML
-    public Button clearNewUserFieldButton;
     @FXML
-    private Button cancelNewUserButton;
+    private ImageView personImage;
+    @FXML
+    private Button photoOpenButton;
+    @FXML
+    private Button cancelSaveUserButton;
     @FXML
     private TextField phoneNewContactField;
     @FXML
@@ -31,21 +38,54 @@ public class CreateNewContactController {
     private DatePicker birthdayDataPicker;
     @FXML
     private TextArea noteTextArea;
+    private Person newPerson;
 
-    public void createNewUser(ActionEvent event) {
-        Person newPerson = new Person(surnameNewContactField.getText(), nameNewContactField.getText(), middlnameNewContactField.getText(), phoneNewContactField.getText(),
-                emailNewContactField.getText(), addressNewContactField.getText(), birthdayDataPicker.getValue().toString(), noteTextArea.getText());
-        CollectionContacts.addContact(newPerson);
-        closeNewUserWindow(event);
+    public Person getNewPerson() {
+        return newPerson;
+    }
+    public void setPerson(Person person) {
+        if (person != null) {
+            this.newPerson = person;
+            surnameNewContactField.setText(newPerson.getSurname());
+            nameNewContactField.setText(newPerson.getName());
+            middlnameNewContactField.setText(newPerson.getMiddleName());
+            phoneNewContactField.setText(newPerson.getPhoneNumber());
+            emailNewContactField.setText(newPerson.getEmail());
+            addressNewContactField.setText(newPerson.getAddress());
+            noteTextArea.setText(newPerson.getPersonNote());
+            birthdayDataPicker.setValue(ConvertData.convertStringToLocalDate(newPerson.getBirthday()));
+            personImage.setImage(ConvertData.convertToImage(newPerson.getPersonImage()));
+        }
     }
 
-    public void closeNewUserWindow(ActionEvent event) {
-        Stage newContactStage = (Stage) cancelNewUserButton.getScene().getWindow();
-        newContactStage.close();
+    public void saveContact(ActionEvent event) {
+        //Исправить после подключения БД
+        this.newPerson.setPersonID(CollectionContacts.getPersonsList().size());
+        //--------
+        this.newPerson.setSurname(surnameNewContactField.getText() == null ? " " : surnameNewContactField.getText());
+        this.newPerson.setName(nameNewContactField.getText() == null ? " " : nameNewContactField.getText());
+        this.newPerson.setMiddleName(middlnameNewContactField.getText() == null ? " " : middlnameNewContactField.getText());
+        this.newPerson.setPhoneNumber(phoneNewContactField.getText() == null ? " " : phoneNewContactField.getText());
+        this.newPerson.setEmail(emailNewContactField.getText() == null ? " " : emailNewContactField.getText());
+        this.newPerson.setAddress(addressNewContactField.getText() == null ? " " : addressNewContactField.getText());
+        this.newPerson.setBirthday(birthdayDataPicker.getValue() == null ? " " : ConvertData.convertLocalDateToString(birthdayDataPicker.getValue()));
+        this.newPerson.setPersonNote(noteTextArea.getText() == null ? " " : noteTextArea.getText());
+        ContactsController.setAddPerson(true);
+        hideWindow();
     }
 
+    public void cancelSaveContact(ActionEvent event) {
+        ContactsController.setAddPerson(false);
+        hideWindow();
+    }
 
-    public void clearFields(ActionEvent event) {
+    private void hideWindow() {
+        Stage newContactStage = (Stage) cancelSaveUserButton.getScene().getWindow();
+        clearFields();
+        newContactStage.hide();
+    }
+
+    public void clearFields() {
         surnameNewContactField.setText("");
         nameNewContactField.setText("");
         middlnameNewContactField.setText("");
@@ -54,7 +94,26 @@ public class CreateNewContactController {
         addressNewContactField.setText("");
         birthdayDataPicker.setValue(null);
         noteTextArea.setText("");
+        personImage.setImage(null);
     }
 
-
+    public void addPhotoButton(ActionEvent event) {
+        final FileChooser choose = new FileChooser();
+        photoOpenButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                choose.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("All Images", "*.*"),
+                    new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                    new FileChooser.ExtensionFilter("PNG", "*.png")
+                );
+                File file = choose.showOpenDialog(photoOpenButton.getScene().getWindow());
+                if (file != null) {
+                    byte[] bImage = ConvertData.convertToByteArray(file);
+                    newPerson.setPersonImage(bImage);
+                    personImage.setImage(ConvertData.convertToImage(bImage));
+                }
+            }
+        });
+    }
 }
