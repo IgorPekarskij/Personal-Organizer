a01b6be18ef48482d8e144b994202b3a439de70d
Igor Pekarskij <igorpekarskij.brest@gmail.com>
2016-12-18 20:36:33 +0300

    Added method that converted byte array to Image and File to byte array

---
+++ Organizer/src/main/java/utils/ConvertData.java
@@ -0,0 +1,67 @@
+package utils;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class ConvertData {
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_PATTERN);
+
+    public static LocalDate convertStringToLocalDate (String birthday) {
+        if (birthday != null && !birthday.isEmpty() && birthday != " ") {
+            try {
+                return DATE_FORMATTER.parse(birthday, LocalDate::from);
+            } catch (DateTimeParseException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public static String convertLocalDateToString(LocalDate birthday) {
+        if(birthday == null) {
+            return null;
+        }
+        return DATE_FORMATTER.format(birthday);
+    }
+
+    public static Image convertToImage (Object source) {
+        Image result = null;
+        if (source instanceof byte[]) {
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream((byte[])source);
+            result = new Image(byteArrayInputStream, 500, 350, false, false);
+        }
+        return result;
+    }
+
+    public static byte[] convertToByteArray (File source) {
+        byte[] result = new byte[(int) source.length()];
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(source);
+            if(fis.read(result) == -1){
+
+            }
+        }
+        catch (FileNotFoundException e) {
+            Alert fnfe = new Alert(Alert.AlertType.ERROR);
+            fnfe.setContentText("Невозможно открыть файл!");
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                fis.close();
+            } catch (IOException NOP) {
+
+            }
+        }
+        return result;
+    }
+}
