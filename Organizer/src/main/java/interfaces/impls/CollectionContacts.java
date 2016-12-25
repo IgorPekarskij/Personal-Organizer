034a375c2cf51bbc62be4a753484563ff6c63120
igor <igorpekarskij.brest@gmail.com>
2016-12-25 23:12:30 +0300

    Moved method for Note object into CollectionNote class

--- Organizer/src/main/java/interfaces/impls/CollectionContacts.java
+++ Organizer/src/main/java/interfaces/impls/CollectionContacts.java
@@ -3,19 +3,11 @@ package interfaces.impls;
 import interfaces.IContact;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import objects.Note;
 import objects.Person;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import testData.TestData;
 
 public class CollectionContacts implements IContact {
-    private String surname;
-    private String name;
-    private String middlename;
-    private String phoneNumber;
-    private String email;
-    private String address;
-    private String birthday;
-    private String personNote;
     private static ObservableList<Person> personsList = FXCollections.observableArrayList();
 
     public static void addContact(Person person) {
@@ -30,19 +22,15 @@ public class CollectionContacts implements IContact {
         personsList.remove(person);
     }
 
-    public void fillList(){
-        SimpleDateFormat date = new SimpleDateFormat("dd.MM.yyyy");
-        for (int i = 1; i <= 10; i++) {
-            surname = "Petrov" + i;
-            name = "Ivan" + i;
-            middlename = "Ivanovich" + i;
-            phoneNumber = "40000" + i;
-            email = i + "test@mail.ru";
-            address = "Brest, Kirova, " + i;
-            birthday = date.format(new Date(System.currentTimeMillis()));
-            personNote = "Test" + i;
-            personsList.add(new Person(i, surname, name, middlename, phoneNumber, email, address, birthday, personNote));
+    public void fillContactList(){
+        if (personsList.size() == 0) {
+            personsList = TestData.createContacts();
         }
+        System.out.println(personsList.size());
+    }
+
+    public void setNotesList(ObservableList<Note> notesList) {
+        CollectionNotes.notesList = notesList;
     }
 
     public static ObservableList<Person> getPersonsList() {
@@ -52,5 +40,4 @@ public class CollectionContacts implements IContact {
     public void setPersonsList(ObservableList<Person> personsList) {
         this.personsList = personsList;
     }
-
 }
