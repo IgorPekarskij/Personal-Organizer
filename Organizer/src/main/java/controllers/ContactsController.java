b4e88ed1c11531c8d234f2765829ce231b83557c
igor <igorpekarskij.brest@gmail.com>
2016-12-25 23:18:25 +0300

    Implemented all functionality for contact window controls.

--- Organizer/src/main/java/controllers/ContactsController.java
+++ Organizer/src/main/java/controllers/ContactsController.java
@@ -1,53 +1,222 @@
 package controllers;
 
 import interfaces.impls.CollectionContacts;
-import javafx.collections.ListChangeListener;
+import javafx.beans.binding.Bindings;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import objects.Person;
-
+import utils.ConvertData;
 import java.io.IOException;
 
 public class ContactsController {
-    CollectionContacts collectionContacts = new CollectionContacts();
+    private CollectionContacts collectionContacts = new CollectionContacts();
+    private static Person currentPerson;
+    private Parent fxmlEdit;
+    private FXMLLoader fxmlLoader;
+    private CreateNewContactController createNewContact;
+    private Stage editDialogStage;
+    private static boolean addPerson = true;
     @FXML
-    public TableView fullListOfContact;
+    private TextField contactSearchField;
     @FXML
-    public TableColumn fioColumn;
+    private ImageView contactImage;
     @FXML
-    public TableColumn phoneColumn;
+    public Button updateContactButton;
     @FXML
-    public Label countFoundRecords;
+    private Label fioCurrentContact;
     @FXML
-    private void initialize(){
-        fioColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("FIO"));
+    private Label telephoneCurrentContact;
+    @FXML
+    private Label emailCurrentContact;
+    @FXML
+    private Label addressCurrentContact;
+    @FXML
+    private Label birthdayCurrentContact;
+    @FXML
+    private TextArea noteCurrentContact;
+    @FXML
+    private TableView listOfContact;
+    @FXML
+    private TableColumn nameColumn;
+    @FXML
+    private TableColumn surnameColumn;
+    @FXML
+    private TableColumn phoneColumn;
+    @FXML
+    private Label countFoundRecords;
+
+    @FXML
+    private void initialize() {
+        //Fill table.
+        surnameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("surname"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("name"));
         phoneColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("PhoneNumber"));
-        collectionContacts.getPersonsList().addListener(new ListChangeListener<Person>() {
+        initListeners();
+        initLoader();
+        fillData();
+
+        FilteredList <Person> filteredList = new FilteredList<>(collectionContacts.getPersonsList(), p -> true);
+        contactSearchField.textProperty().addListener(((observable, oldValue, newValue) -> {
+            filteredList.setPredicate(person -> {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                String lowerCaseFilter = newValue.toLowerCase();
+                if (person.getSurname().toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter matches first name.
+                } else if (person.getName().toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter matches last name.
+                } else if (person.getPhoneNumber().toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter matches phone.
+                }
+                return false; // Does not match.
+            });
+        }) );
+
+        SortedList<Person> sortedList = new SortedList<Person>(filteredList);
+        sortedList.comparatorProperty().bind(listOfContact.comparatorProperty());
+        listOfContact.setItems(sortedList);
+        updateCountLabel();
+    }
+
+    private void fillData() {
+        collectionContacts.fillContactList();
+        listOfContact.setItems(collectionContacts.getPersonsList());
+        listOfContact.getSelectionModel().selectFirst();
+    }
+
+    private void initListeners() {
+        //Displays information to current contact
+        listOfContact.getSelectionModel().selectedItemProperty().addListener(
+                ((observable, oldValue, newValue) -> showPersonDetail(newValue))
+        );
+        //Open edit window by double click on current contact
+        listOfContact.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
-            public void onChanged(Change<? extends Person> c) {
-                updateCountLabel();
+            public void handle(MouseEvent event) {
+                if(event.getClickCount() == 2) {
+                    createNewContact.setPerson((Person) listOfContact.getSelectionModel().getSelectedItem());
+                    editDialogStage = showWindow(editDialogStage, listOfContact, fxmlEdit);
+                    showPersonDetail((Person) listOfContact.getSelectionModel().getSelectedItem());
+                }
             }
         });
-        collectionContacts.fillList();
-        fullListOfContact.setItems(collectionContacts.getPersonsList());
+    }
+
+    private void initLoader() {
+        try {
+            fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/fxmls/newContactWindow.fxml"));
+            fxmlEdit = fxmlLoader.load();
+            createNewContact = fxmlLoader.getController();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showPersonDetail(Object person) {
+        if (person == null) return;
+        currentPerson = (Person) person;
+        fioCurrentContact.setText(currentPerson.getSurname() + " " + currentPerson.getName() + " " + currentPerson.getMiddleName());
+        telephoneCurrentContact.setText(currentPerson.getPhoneNumber());
+        emailCurrentContact.setText(currentPerson.getEmail());
+        addressCurrentContact.setText(currentPerson.getAddress());
+        birthdayCurrentContact.setText(currentPerson.getBirthday());
+        noteCurrentContact.setText(currentPerson.getPersonNote());
+        Image image = ConvertData.convertToImage(currentPerson.getPersonImage());
+        if(image != null) {
+            contactImage.setImage(image);
+            AnchorPane pane = (AnchorPane) contactImage.getParent();
+            pane.setCenterShape(true);
+        }
     }
 
     public void createNewContact(ActionEvent event) throws IOException {
-        WelcomeWindowController createContact = new WelcomeWindowController();
-        createContact.openNewContact(event);
+        Object currentEvent = event.getSource();
+        if(!(currentEvent instanceof Button)){
+            return;
+        }
+        Button currentButton = (Button) currentEvent;
+        switch (currentButton.getId()) {
+            case "newContactButton" :
+                System.out.println(addPerson);
+                createNewContact.setPerson(new Person());
+                editDialogStage = showWindow(editDialogStage, listOfContact, fxmlEdit);
+                System.out.println(addPerson);
+                if (addPerson) {
+                    collectionContacts.addContact(createNewContact.getNewPerson());
+                    listOfContact.getSelectionModel().selectLast();
+                }
+                break;
+            case "updateContactButton" :
+                createNewContact.setPerson((Person) listOfContact.getSelectionModel().getSelectedItem());
+                editDialogStage = showWindow(editDialogStage, listOfContact, fxmlEdit);
+                showPersonDetail((Person)listOfContact.getSelectionModel().getSelectedItem());
+                break;
+        }
     }
 
     public void deleteContact(ActionEvent event) {
-        int selectedIndex = fullListOfContact.getSelectionModel().getSelectedIndex();
-        CollectionContacts.deleteContact(selectedIndex);
+        CollectionContacts.deleteContact((Person) listOfContact.getSelectionModel().getSelectedItem());
     }
 
     private void updateCountLabel(){
-        countFoundRecords.setText("Найдено " + collectionContacts.getPersonsList().size() + " записей");
+        countFoundRecords.textProperty().bind(Bindings.size(listOfContact.getItems()).asString("Найдено %s записей"));
     }
 
+    public static Person getCurrentPerson() {
+        return currentPerson;
+    }
+
+    public Stage showWindow(Stage stage, TableView tableView, Parent fxmlEdit) {
+        if(stage == null) {
+            System.out.println("New Stage");
+            stage = new Stage();
+            stage.setTitle("Organizer");
+            stage.setResizable(false);
+            stage.setScene(new Scene(fxmlEdit));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(tableView.getParent().getScene().getWindow());
+        }
+        stage.showAndWait();
+        return stage;
+    }
+
+    public static void setAddPerson(boolean addPerson) {
+        ContactsController.addPerson = addPerson;
+    }
+
+    public void exitApplication(ActionEvent event) {
+       LoginWindowController.exitApplication();
+    }
+
+
+    public void openContacts(ActionEvent event) throws IOException{
+        Parent contactScene = FXMLLoader.load(getClass().getResource("/fxmls/contactsWindow.fxml"));
+        WelcomeWindowController.openNewWindow(listOfContact, contactScene);
+    }
+
+    public void openTasks(ActionEvent event) throws IOException{
+        Parent contactScene = FXMLLoader.load(getClass().getResource("/fxmls/tasksWindow.fxml"));
+        WelcomeWindowController.openNewWindow(listOfContact, contactScene);
+    }
+
+    public void openNotes(ActionEvent event) throws IOException{
+        Parent contactScene = FXMLLoader.load(getClass().getResource("/fxmls/notesWindow.fxml"));
+        WelcomeWindowController.openNewWindow(listOfContact, contactScene);
+    }
 }
