4d4c0c490f0d6280ef15b2355933819b77f8120c
igor <igorpekarskij.brest@gmail.com>
2017-03-10 22:16:45 +0300

    Added handling null pointer exception while importing contacts. Added counting imported contacts and information message with this information.

--- Organizer/src/main/java/utils/VcardFactory.java
+++ Organizer/src/main/java/utils/VcardFactory.java
@@ -54,26 +54,37 @@ public class VcardFactory {
     public static Contact parseVcard (VCard vCard) {
         Contact newContact = new Contact();
         StructuredName structuredName = vCard.getStructuredName();
-        //Add checking FIO on NULL
+        if (structuredName.getFamily() == null && structuredName.getGiven() == null && structuredName.getAdditional().size() == 0) {
+            return null;
+        }
         newContact.setSurname(structuredName.getFamily() == null ? structuredName.getGiven() : structuredName.getFamily());
         newContact.setName(structuredName.getGiven() == null ? "" : structuredName.getGiven());
         newContact.setMiddleName(structuredName.getAdditional().size() == 0 ? "" : structuredName.getAdditional().get(0));
-        newContact.setPhoneNumber(vCard.getTelephoneNumbers().size() == 0 ? "" : vCard.getTelephoneNumbers().get(0).getText());
-        newContact.setEmail(vCard.getEmails().size() == 0? "" : vCard.getEmails().get(0).getValue());
+
+        if (vCard.getTelephoneNumbers() != null) {
+            newContact.setPhoneNumber(vCard.getTelephoneNumbers().size() == 0 ? "" : vCard.getTelephoneNumbers().get(0).getText());
+        }
+        if (vCard.getEmails() != null) {
+            newContact.setEmail(vCard.getEmails().size() == 0 ? "" : vCard.getEmails().get(0).getValue());
+        }
         List<Address> address = vCard.getAddresses();
         if (address.size() > 0) {
             newContact.setCountry(address.get(0).getCountry());
             newContact.setCity(address.get(0).getLocality());
             newContact.setAddress(address.get(0).getStreetAddress());
         }
-        Date date = vCard.getBirthday() == null? null : vCard.getBirthday().getDate();
+        Date date = vCard.getBirthday() == null ? null : vCard.getBirthday().getDate();
         if (date != null) {
             String birthday = ConvertData.convertLocalDateToString((date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()));
             newContact.setBirthday(birthday);
         }
-        newContact.setPersonNote(vCard.getNotes().size() == 0 ? "" : vCard.getNotes().get(0).getValue());
-        byte[] photo = vCard.getPhotos().size() == 0 ? null : vCard.getPhotos().get(0).getData();
-        newContact.setPersonImage(photo);
+        if (vCard.getNotes() != null) {
+            newContact.setPersonNote(vCard.getNotes().size() == 0 ? "" : vCard.getNotes().get(0).getValue());
+        }
+        if (vCard.getPhotos() != null) {
+            byte[] photo = vCard.getPhotos().size() == 0 ? null : vCard.getPhotos().get(0).getData();
+            newContact.setPersonImage(photo);
+        }
         return newContact;
     }
 }
